9477350732aee8883a7b7e8e10bd535601c070a1
zero <admin@siracprogramming.de>
2018-12-03 21:28:22 +0100

    communication via usb

--- src/g13_device.rs
+++ src/g13_device.rs
@@ -1,3 +1,6 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::time::Duration;
+
 pub struct G13Device<'a> {
     device: libusb::Device<'a>,
     handle: libusb::DeviceHandle<'a>
@@ -19,10 +22,46 @@ impl<'a> G13Device<'a> {
         }
         handle.claim_interface(interface)?;
 
-        Ok(G13Device {
+        let device = G13Device {
             device: device,
             handle: handle
-        })
+        };
+
+        device.set_mode_leds(0);
+        device.set_led_color(0, 255, 255);
+        info!("Initialized {}", device);
+
+        Ok(device)
+    }
+
+    pub fn set_mode_leds(&self, leds: i32) {
+        let usb_data = [5, leds as u8, 0, 0, 0];
+
+        self.handle.write_control(libusb::request_type(libusb::Direction::Out, libusb::RequestType::Class, libusb::Recipient::Interface),
+                9,
+                0x305,
+                0,
+                &usb_data,
+                Duration::from_secs(1)
+        ).unwrap();
+    }
+
+    pub fn set_led_color(&self, red: u8, green: u8, blue: u8) {
+        let usb_data = [5, red, green, blue, 0];
+
+        self.handle.write_control(libusb::request_type(libusb::Direction::Out, libusb::RequestType::Class, libusb::Recipient::Interface),
+                9,
+                0x307,
+                0,
+                &usb_data,
+                Duration::from_secs(1)
+        ).unwrap();
+    }
+}
+
+impl<'a> Display for G13Device<'a> {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "G13Device(Bus: {:03}, Address: {:03})", self.device.bus_number(), self.device.address())
     }
 }
 
