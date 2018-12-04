190c37975a819f73603751517b0201a651d6ab99
zero <admin@siracprogramming.de>
2018-12-04 01:50:08 +0100

    added more keys

--- src/g13_key.rs
+++ src/g13_key.rs
@@ -28,7 +28,7 @@ pub struct G13KeyInformation {
     pub name: &'static str
 }
 
-pub const G13_KEYS: [G13KeyInformation; 22] = [
+pub const G13_KEYS: [G13KeyInformation; 40] = [
     G13KeyInformation { index: 0, name: "G1" },
     G13KeyInformation { index: 1, name: "G2" },
     G13KeyInformation { index: 2, name: "G3" },
@@ -51,5 +51,23 @@ pub const G13_KEYS: [G13KeyInformation; 22] = [
     G13KeyInformation { index: 19, name: "G20" },
     G13KeyInformation { index: 20, name: "G21" },
     G13KeyInformation { index: 21, name: "G22" },
+    G13KeyInformation { index: 22, name: "UNDEF1" },
+    G13KeyInformation { index: 23, name: "LIGHT_STATE" },
+    G13KeyInformation { index: 24, name: "BD" },
+    G13KeyInformation { index: 25, name: "L1" },
+    G13KeyInformation { index: 26, name: "L2" },
+    G13KeyInformation { index: 27, name: "L3" },
+    G13KeyInformation { index: 28, name: "L4" },
+    G13KeyInformation { index: 29, name: "M1" },
+    G13KeyInformation { index: 30, name: "M2" },
+    G13KeyInformation { index: 31, name: "M3" },
+    G13KeyInformation { index: 32, name: "MR" },
+    G13KeyInformation { index: 33, name: "LEFT" },
+    G13KeyInformation { index: 34, name: "DOWN" },
+    G13KeyInformation { index: 35, name: "TOP" },
+    G13KeyInformation { index: 36, name: "UNDEF3" },
+    G13KeyInformation { index: 37, name: "LIGHT" },
+    G13KeyInformation { index: 38, name: "LIGHT2" },
+    G13KeyInformation { index: 39, name: "MISC_TOGGLE" },
 ];
 
